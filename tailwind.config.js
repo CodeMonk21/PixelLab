3e0de26d12081d982f954624e08f3ffb21387cf7
CodeMonk21 <jagritmadaan2001@gmail.com>
2023-09-01 22:27:44 +0530

    setup custom font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content:["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'lato': ['Lato', 'sans-serif']
+      },
+    },
   },
   plugins: [],
 }
