d001e1092ff2fee08d67181410fc48446ed677d9
CodeMonk21 <jagritmadaan2001@gmail.com>
2023-09-02 18:16:31 +0530

    custom them added

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content:["./public/**/*.{html,js}"],
   theme: {
     extend: {
+      gridTemplateColumns:{
+        'auto-2':'repeat(2,auto)',
+      },
       fontFamily: {
         'lato': ['Lato', 'sans-serif']
       },
